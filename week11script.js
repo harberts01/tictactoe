0b0876e5eb0b0b3d0751b4e7fd12a7a00962c4ea
harberts01 <109708014+harberts01@users.noreply.github.com>
2022-10-14 23:46:39 -0500

    Modified modal to timeout, added win scenario action

--- week11script.js
+++ week11script.js
@@ -1,9 +1,10 @@
 
 const button = document.querySelectorAll("#gameBtn");
-const restartBtn = document.querySelector("  #restartButton")
+const restartBtn = document.querySelector("  #restartButton");
 const cell = document.querySelector("#cell");
 const modalHeader = document.querySelector("#ModalLabel");
-let currentPlayer = "X"
+const modal = document.querySelector("#Modal");
+let currentPlayer = "X";
 
 //possible win situations for cellIndexes.
 let winConditions = [
@@ -15,7 +16,7 @@ let winConditions = [
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6]
-]
+];
 //player arrays are filled when a button is clicked
 let playerO = [];
 let playerX = [];
@@ -31,30 +32,39 @@ function playGame(){
         let index = button.getAttribute("cellIndex");//retrieves index attribute given to buttons in HTML so that it can be logged
             if(currentPlayer == "O"){//this if statement manipulates the buttons when it is O's turn; also pushes index numbers to correct player arrays.
                 button.className = 'btn btn-lrg btn-danger w-50 m-5'
-                playerO.push(index);
+                playerO.push(Number(index));
             }else{
-                playerX.push(index);
+                playerX.push(Number(index));
             };
-        console.log(playerO)
+        window.setTimeout(function(){
+            $('#Modal').modal('hide');
+        }, 1100);
+        
+        winCheck(playerO)
+        winCheck(playerX)
         changePlayer();//this will alternate players after a move is selected and also update Modal text
         
     }));
 
-    winCheck(playerO);
-    winCheck(playerX); 
-    console.log(winCheck(playerO));
-    console.log(winCheck(playerX));
 
 };
 
 
 
-console.log(playerO);
-console.log(playerX);
+
+
 
 function changePlayer(){
     currentPlayer = (currentPlayer == "X") ? "O" : "X";
-    modalHeader.textContent = `It's ${currentPlayer}'s Turn!`  
+    modalHeader.textContent = `It's ${currentPlayer}'s Turn!`
+    if(winCheck(playerO) === true){
+        window.location.replace("winner.html")
+    }else if(winCheck(playerX) === true){
+        window.location.replace("winnerX.html")
+    }else if(playerO.length === 4 && playerX.length === 5){
+        modalHeader.textContent = `It's a Tie! Let's Play Again!`
+    }
+      
 };
 
 //this function will create a win scenario. win will = true when the if statement conditions
@@ -62,9 +72,27 @@ function changePlayer(){
 
 
 function winCheck(playerArry){
+    
      for(let i = 0; i < winConditions.length; i++){
+        
         if (winConditions[i].every(elem => playerArry.includes(elem))){
             return true
-        }
-    }return false;
+
+        }        
+    } return false;
 }
+
+
+
+
+
+restartBtn.addEventListener("click", () => {
+    window.location.replace("game.html")
+});
+
+
+
+
+    
+
+
